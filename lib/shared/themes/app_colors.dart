78960ab60cd23759bd5bafa88433301d3fe7af97
priolilucas1 <priolilucas1@gmail.com>
2021-06-30 10:08:47 -0300

    Colors Styles Added

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primaryTextColor = Color(0xFF414C6B);
+  static final secondaryTextColor = Color(0xFFE4979E);
+  static final titleTextColor = Colors.white;
+  static final contentTextColor = Color(0xff868686);
+  static final navigationColor = Color(0xFF6751B5);
+  static final gradientStartColor = Color(0xFF0050AC);
+  static final gradientEndColor = Color(0xFF9354B9);
+  static final dropDownText = Color(0x7CDBF1FF);
+}
